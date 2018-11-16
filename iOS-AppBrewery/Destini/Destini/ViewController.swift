a550f5d1fb25cd775d705fbe0869cc5becdc63ba
h01m3s <lin426797@gmail.com>
2018-11-15 23:25:07 -0500

    Destiti Complete.

--- iOS-AppBrewery/Destini/Destini/ViewController.swift
+++ iOS-AppBrewery/Destini/Destini/ViewController.swift
@@ -33,10 +33,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
     
     // TODO Step 5: Initialise instance variables here
     
-    
+    var storyIndex = 1
     
     
     override func viewDidLoad() {
@@ -44,7 +45,7 @@ class ViewController: UIViewController {
         
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        restart()
     }
 
     
@@ -52,10 +53,48 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
+        
+        if sender.tag == 1 && (storyIndex == 1 || storyIndex == 2){
+            updateStoryAndButtons(storyText: story3, topButtonTitle: answer3a, bottomButtonTitle: answer3b, nextIndex: 3)
+        } else if sender.tag == 2 && storyIndex == 1 {
+            updateStoryAndButtons(storyText: story2, topButtonTitle: answer2a, bottomButtonTitle: answer2b, nextIndex: 2)
+        } else if sender.tag == 1 && storyIndex == 3 {
+            updateStoryAndButtons(storyText: story6, topButtonTitle: "", bottomButtonTitle: "", nextIndex: 6)
+        } else if sender.tag == 2 && storyIndex == 3 {
+            updateStoryAndButtons(storyText: story5, topButtonTitle: "", bottomButtonTitle: "", nextIndex: 5)
+        } else if sender.tag == 2 && storyIndex == 2 {
+            updateStoryAndButtons(storyText: story4, topButtonTitle: "", bottomButtonTitle: "", nextIndex: 4)
+        }
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
         
     
+    }
+    
+    @IBAction func restartPressed(_ sender: UIButton) {
+        restart()
+    }
+    
+    fileprivate func restart() {
+        updateStoryAndButtons(storyText: story1, topButtonTitle: answer1a, bottomButtonTitle: answer1b, nextIndex: 1)
+        restartButton.isHidden = true
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+    }
+    
+    
+    fileprivate func updateStoryAndButtons(storyText: String, topButtonTitle: String, bottomButtonTitle: String, nextIndex: Int) {
+        storyTextView.text = storyText
+        if nextIndex < 4  {
+            topButton.setTitle(topButtonTitle, for: .normal)
+            bottomButton.setTitle(bottomButtonTitle, for: .normal)
+        } else {
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButton.isHidden = false
+        }
+        
+        storyIndex = nextIndex
     }
     
 
